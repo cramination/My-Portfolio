84ba567359c5ce3c7e9193dec8ee72beaaa2b293
cramination <camnation@comcast.net>
2025-10-17 16:26:41 -0400

    second commit

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ["**/*.riv", "**/*.glb"],
-  base: '/My-Portfolio/'
+  base: '/My-Portfolio/',
 })
